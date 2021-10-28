dfeb569e3eb2a1b1276b0f71223805de9a917d2a
Albert Hakobyan <abhakobyan24@gmail.com>
2021-10-28 12:00:22 +0400

    Добавьте файлы проекта.

---
+++ BirthdayCakeCandles_HackerRank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCakeCandles_HackerRank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 2,5,6,6,5,4,6,2,3,4,6,4};
+            int x = birthdayCakeCandles(list);
+            Console.WriteLine(x);
+        }
+
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int max = candles.Max();
+            int count = 0;
+            for (int i = 0; i < candles.Count; i++)
+            {
+                if (max == candles[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
